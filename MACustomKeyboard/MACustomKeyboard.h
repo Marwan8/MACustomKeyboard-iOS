bb8f93378e3fcb9a52778cbca03c9e00d8e2db32
MARWAN <atoz@atozs-macbook-pro.local>
2016-09-11 12:03:58 +0400

    Adds MACustomKeyboard with Blocks

---
+++ MACustomKeyboard/MACustomKeyboard.h
@@ -0,0 +1,28 @@
+//
+//  MACustomKeyboard.h
+//  MACustomKeyboard
+//
+//  Created by MARWAN on 4/30/16.
+//  Copyright © 2016 MARWAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@class MACustomKeyboard;
+typedef void (^NumberTappedBlock) (MACustomKeyboard *view, NSString *text);
+typedef void (^DoneTappedBlock) (MACustomKeyboard *view);
+typedef void (^ClearTappedBlock) (MACustomKeyboard *view);
+
+@interface MACustomKeyboard : UIView
+
+@property (nonatomic,retain) NSArray * keys;
+@property (nonatomic, copy) NumberTappedBlock numberTappedBlock;
+@property (nonatomic, copy) DoneTappedBlock doneTappedBlock;
+@property (nonatomic, copy) ClearTappedBlock clearTappedBlock;
+
+@property (nonatomic) id delegate;
+
+-(void)setKeyboardButtons:(NSArray *)keys;
+- (IBAction)keyboardButtonPressed:(id)sender;
+@end
---
+++ MACustomKeyboard/MACustomKeyboard.m
@@ -0,0 +1,51 @@
+//
+//  MACustomKeyboard.m
+//  MACustomKeyboard
+//
+//  Created by MARWAN on 4/30/16.
+//  Copyright © 2016 MARWAN. All rights reserved.
+//
+
+#import "MACustomKeyboard.h"
+
+@implementation MACustomKeyboard
+
+-(void)setKeyboardButtons:(NSArray *)keys{
+    self.keys = keys;
+    for(UIView * view in self.subviews){
+        if([view isKindOfClass:[UIButton class]])
+        {
+            UIButton * keyboardBtn = (UIButton *) view;
+            if ([keys[keyboardBtn.tag - 1] isKindOfClass:[NSString class]]) {
+                [keyboardBtn setTitle:keys[keyboardBtn.tag - 1] forState:UIControlStateNormal];
+            }
+        }
+    }
+    
+}
+
+
+- (IBAction)keyboardButtonPressed:(id)sender {
+    UIButton * btn = (UIButton *)sender;
+    NSString * text = btn.currentTitle;
+    
+    if (self.numberTappedBlock != nil) {
+        self.numberTappedBlock(self, text);
+    }
+}
+
+- (IBAction)doneButtonPressed:(id)sender {
+    
+    if (self.doneTappedBlock != nil) {
+        self.doneTappedBlock(self);
+    }
+}
+
+- (IBAction)clearButtonPressed:(id)sender {
+    
+    if (self.clearTappedBlock != nil) {
+        self.clearTappedBlock(self);
+    }
+}
+
+@end
